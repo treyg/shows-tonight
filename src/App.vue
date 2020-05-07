a04ef61c79f517fef2a4cacbf6258cad25951794
treyg21 <41927921+treyg21@users.noreply.github.com>
2020-05-07 15:45:02 -0400

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <GRShows />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import GRShows from './components/GRShows.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    GRShows
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import "https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"
 </style>
---
+++ src/components/GRShows.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="container">
+    <div class="col s12 m7">
+          <h4 class="header">Upcoming Shows </h4>
+          <div class="card horizontal" v-for="event in events" :key="event">
+           
+            <div class="card-stacked">
+              <div class="card-content">
+
+                 <a :href="event.performance[0].artist.uri">
+                   <h5 class="heading h5">{{ event.performance[0].displayName }}
+                     <span> - {{ event.start.date }}</span>
+                     </h5>
+                 </a>
+                    <h6 class="">{{ event.venue.displayName }} - 
+                      <span> {{ event.venue.metroArea.displayName }}</span>
+                    </h6>
+        
+              </div>
+              <div class="card-action">
+                <a class="red-text text-darken-2" :href="event.uri">View Event Page</a>
+              </div>
+            </div>
+          </div>
+        </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GRShows',
+  data: function() { 
+           return { // test songkick api key
+            apiUrl: 'https://api.songkick.com/api/3.0',
+            apiKey: 'eMoOYBIe41vUwDWU',
+            location: '5035',
+            events: null,
+            dates: null
+           }
+          }, 
+    
+          methods: {
+            fetchEvents: function() {
+              const url = `${this.apiUrl}/metro_areas/${this.location}/calendar.json?apikey=${this.apiKey}`;
+    
+              fetch(url)
+                .then(response => response.json())
+                .then(data => {
+                  this.events = data.resultsPage.results.event
+                  this.dates = data.resultsPage.results.event
+                });
+            },
+  
+          },
+    
+          created: function() {
+            this.fetchEvents();
+          }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
