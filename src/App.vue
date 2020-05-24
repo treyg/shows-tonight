b8df38f14caa6f32bd31a53958dc1defbbe14695
Taylor <taylorwteaches@gmail.com>
2020-05-24 12:22:18 -0400

    removed search from nav for now

--- src/App.vue
+++ src/App.vue
@@ -14,14 +14,6 @@
                 <router-link class="text-gray" to="/blog">Blog</router-link>
               </b-nav-item>
             </b-navbar-nav>
-
-            <!-- Right aligned nav items -->
-            <b-navbar-nav class="ml-auto" right>
-              <b-nav-form>
-                <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-                <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-              </b-nav-form>
-            </b-navbar-nav>
           </b-collapse>
         </b-navbar>
       </div>
